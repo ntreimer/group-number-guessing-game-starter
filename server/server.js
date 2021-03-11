13fd52adfa5702dd2d1d84f5a6c8dac9fd80bfcf
Nate Treimer <natetreimer@gmail.com>
2021-03-11 13:56:10 -0600

    added input fields and a button, randomnumber

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
 
+let randomNumberGenerator = Math.floor(Math.random() * 26)
+
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
 
